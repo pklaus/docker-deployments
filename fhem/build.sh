46b2e626219707739f976f95b2472ac88e023d4b
Philipp Klaus <philipp.klaus@gmail.com>
2018-02-11 19:31:05 +0100

    FHEM: new tag including the GIT commit hash & assert repo clean

--- fhem/build.sh
+++ fhem/build.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-docker build -t pklaus/fhem:5.8 --squash .
+# check if Git repo clean (untracked files are ignored):
+git diff-index --quiet HEAD
+if [ $? -ne 0 ]; then
+  >&2 echo "Uncommited changes. Not building..."
+  exit 1
+fi
+
+# fail if any command fails
+set -e
+
+COMMIT=$(git rev-parse --verify HEAD)
+SHORT_COMMIT=$(echo $COMMIT | cut -c 1-7)
+
+docker build -t pklaus/fhem:latest --squash .
+docker tag pklaus/fhem:latest pklaus/fhem:5.8
+docker tag pklaus/fhem:latest pklaus/fhem:5.8_${SHORT_COMMIT}
--- fhem/push.sh
+++ fhem/push.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# fail if any command fails:
+set -e
+
+COMMIT=$(git rev-parse --verify HEAD)
+SHORT_COMMIT=$(echo $COMMIT | cut -c 1-7)
+
+docker push pklaus/fhem:5.8_${SHORT_COMMIT}
 docker push pklaus/fhem:5.8
+docker push pklaus/fhem:latest
